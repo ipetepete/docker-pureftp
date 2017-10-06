0d59851eb1d8392b21beb8645c2adc3c4d755806
Peter Peterson <peter.peterson.iv@gmail.com>
2017-10-06 13:08:08 -0700

    syntax for ports was messed up

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,8 @@ copy ./build /build
 
 run /build/install.sh
 
-CMD /build/run.sh -c 40 -C 40 -P $PUBLICHOST  -l ldap:/etc/pureftpd-ldap.conf -j -R -p 30000-30059
+CMD /build/run.sh -c 40 -C 40 -P $PUBLICHOST -A -G -i -r -k 0 -l ldap:/etc/pureftpd-ldap.conf -j -R -p 60000:60059
 # optional:ly add -tls 1 to enable tls support
 
 
-expose 21 30000-30059
+expose 20 21 30000-30059
--- build/install.sh
+++ build/install.sh
@@ -5,6 +5,12 @@ yum install -y vim zsh git openldap openldap-devel gcc make
 cd /build/pure-ftpd-1.0.46
 make install
 
+yum install -y rsyslog
+echo "" >> /etc/rsyslog.conf
+echo "#PureFTP Custom Logging" >> /etc/rsyslog.conf
+echo "ftp.* /var/log/pure-ftpd/pureftpd.log" >> /etc/rsyslog.conf
+echo "Updated /etc/rsyslog.conf with /var/log/pure-ftpd/pureftpd.log"
+
 mkdir -p /ftp/{users,anon}
 
 # add ftp anon user
