23df3260947967546b45cfdcc7c5fd0b3735fc16
Peter Peterson <peter.peterson.iv@gmail.com>
2018-08-16 08:27:56 -0700

    prepping for test release

--- build/install.sh
+++ build/install.sh
@@ -1,8 +1,12 @@
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum install -y vim zsh git gcc make
+yum install -y vim zsh git gcc make python2-pip
+
+pip install requests
 
 # this should already be compiled, just need to install	
 cd /build/pure-ftpd-1.0.46
+./configure --with-extauth
+make
 make install
 
 yum install -y rsyslog
@@ -13,5 +17,5 @@ mkdir -p /ftp/{users,anon}
 
 # add ftp anon user
 usermod -m -d /ftp/anon ftp
-chown -R ftp:ftp /ftp/anon
+ chown -R ftp:ftp /ftp/anon
 
